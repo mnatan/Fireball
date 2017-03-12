294acb17c4d435213e4c61193b8e8368672059c0
Marcin Natanek <Marcin.Natanek@dreamlab.pl>
2017-03-12 21:43:17 +0100

    js build from maven

--- src/main/java/pl/natsoft/fireball/FireballApplication.java
+++ src/main/java/pl/natsoft/fireball/FireballApplication.java
@@ -8,11 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class FireballApplication {
-    @RequestMapping("/")
-    public String home() {
-        return "Hello World! Test!";
-    }
-
     /**
      * <a href="https://cloud.google`.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking">
      * App Engine health checking</a> requires responding with 200 to {@code /_ah/health}.
